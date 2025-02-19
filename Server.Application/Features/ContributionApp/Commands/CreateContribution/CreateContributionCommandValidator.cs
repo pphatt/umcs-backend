07da7a9feb2993f63bd8f1722746f0268a9a4659
pphatt <vutienphat0809@gmail.com>
2025-02-19 22:47:46 +0700

    feat(contribution): add missing validator for create contribution

--- Server.Application/Features/ContributionApp/Commands/CreateContribution/CreateContributionCommandValidator.cs
+++ Server.Application/Features/ContributionApp/Commands/CreateContribution/CreateContributionCommandValidator.cs
@@ -6,5 +6,24 @@ public class CreateContributionCommandValidator : AbstractValidator<CreateContri
 {
     public CreateContributionCommandValidator()
     {
+        RuleFor(x => x.Title)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Title must not be empty.")
+            .MaximumLength(256)
+            .WithMessage("Title's length must be less than 256.");
+
+        RuleFor(x => x.FacultyId)
+            .NotEmpty()
+            .WithMessage("Faculty ID must not be empty.");
+
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithMessage("User ID must not be empty.");
+
+        RuleFor(x => x.IsConfirmed)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Please accept term and condition before submitting.");
     }
 }
