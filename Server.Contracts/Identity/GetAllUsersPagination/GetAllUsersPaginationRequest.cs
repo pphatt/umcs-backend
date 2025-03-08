360018d6667260db6d439c83aedec84495597e04
pphatt <vutienphat0809@gmail.com>
2025-03-08 15:52:30 +0700

    feat(admin): add OrderBy query params for get all user pagination

--- Server.Contracts/Identity/GetAllUsersPagination/GetAllUsersPaginationRequest.cs
+++ Server.Contracts/Identity/GetAllUsersPagination/GetAllUsersPaginationRequest.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Server.Contracts.Common;
+using Server.Domain.Common.Enums;
 
 namespace Server.Contracts.Identity.GetAllUsersPagination;
 
 public class GetAllUsersPaginationRequest : PaginationRequest
 {
-    [FromQuery]
+    [FromQuery(Name = "facultyName")]
     public string? FacultyName { get; set; }
 
-    [FromQuery]
+    [FromQuery(Name = "roleName")]
     public string? RoleName { get; set; }
+
+    [FromQuery(Name = "orderBy")]
+    public OrderByEnum? OrderBy { get; set; }
 }
