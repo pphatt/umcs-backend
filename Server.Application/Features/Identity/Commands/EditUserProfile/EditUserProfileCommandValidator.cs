d16be0a7c15f782a56bdd56c2b7d428544e9e4e0
pphatt <vutienphat0809@gmail.com>
2025-03-09 21:28:30 +0700

    feat(user): add missing command validator for edit user's profile

--- Server.Application/Features/Identity/Commands/EditUserProfile/EditUserProfileCommandValidator.cs
+++ Server.Application/Features/Identity/Commands/EditUserProfile/EditUserProfileCommandValidator.cs
@@ -1,5 +1,19 @@
-﻿namespace Server.Application.Features.Identity.Commands.EditUserProfile;
+﻿using FluentValidation;
 
-public class EditUserProfileCommandValidator
+namespace Server.Application.Features.Identity.Commands.EditUserProfile;
+
+public class EditUserProfileCommandValidator : AbstractValidator<EditUserProfileCommand>
 {
+    public EditUserProfileCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .MaximumLength(255)
+            .NotEmpty()
+            .WithMessage("First name is required");
+
+        RuleFor(x => x.LastName)
+            .MaximumLength(255)
+            .NotEmpty()
+            .WithMessage("Last name is required");
+    }
 }
