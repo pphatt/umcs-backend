8ac22e30c86c6997fa79d4f1a73771319c7ee56d
pphatt <vutienphat0809@gmail.com>
2025-03-10 14:11:00 +0100

    feat(cron-job): mail manager every 24 hours about ungraded and uncommented contribution

--- Server.Infrastructure/Jobs/JobSetup/JobSetup.cs
+++ Server.Infrastructure/Jobs/JobSetup/JobSetup.cs
@@ -8,15 +8,27 @@ public class JobSetup : IConfigureOptions<QuartzOptions>
 {
     public void Configure(QuartzOptions options)
     {
-        var jobKey = JobKey.Create(nameof(ExperimentalJob));
+        // Experimental Job (5 seconds).
+        var experimentalJobKey = JobKey.Create(nameof(ExperimentalJob));
 
         options
-            .AddJob<ExperimentalJob>(JobBuilder => JobBuilder.WithIdentity(jobKey))
+            .AddJob<ExperimentalJob>(JobBuilder => JobBuilder.WithIdentity(experimentalJobKey))
             .AddTrigger(trigger =>
                 trigger
-                    .ForJob(jobKey)
+                    .ForJob(experimentalJobKey)
                     //.WithCronSchedule("*/1 * * * *")
                     .WithSimpleSchedule(schedule =>
                         schedule.WithIntervalInSeconds(5).RepeatForever()));
+
+        // Mail Manager Job (24 hours).
+        var mailManagerJobKey = JobKey.Create(nameof(MailManagerJob));
+
+        options
+            .AddJob<MailManagerJob>(jobBuilder => jobBuilder.WithIdentity(mailManagerJobKey))
+            .AddTrigger(trigger => 
+                trigger
+                    .ForJob(mailManagerJobKey)
+                    .WithSimpleSchedule(schedule => 
+                        schedule.WithIntervalInMinutes(60).RepeatForever()));
     }
 }
---
+++ Server.Infrastructure/Jobs/MailManagerJob.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Server.Application.Common.Interfaces.Services;
+using Server.Application.Common.Interfaces.Services.Email;
+using Server.Contracts.Common.Email;
+using Server.Domain.Common.Constants.Authorization;
+using Server.Domain.Common.Enums;
+using Server.Domain.Entity.Identity;
+
+namespace Server.Infrastructure.Jobs;
+
+public class MailManagerJob : IJob
+{
+    private readonly AppDbContext _context;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IEmailService _emailService;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<MailManagerJob> _logger;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public MailManagerJob(
+        AppDbContext context,
+        UserManager<AppUser> userManager,
+        IEmailService emailService,
+        IConfiguration configuration,
+        ILogger<MailManagerJob> logger,
+        IDateTimeProvider dateTimeProvider)
+    {
+        _context = context;
+        _userManager = userManager;
+        _emailService = emailService;
+        _configuration = configuration;
+        _logger = logger;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        _logger.LogInformation($"----- Initialize send email to all managers ----- {_dateTimeProvider.UtcNow}");
+
+        var haveAnyContribution = await _context.Contributions
+            .AnyAsync(x => x.IsCoordinatorCommented == false && x.Status == ContributionStatus.Pending);
+
+        var managers = await _userManager.GetUsersInRoleAsync(Roles.Manager);
+
+        var ungradedContributionUrl = _configuration["ApplicationSettings:UngradedContribution"];
+
+        if (haveAnyContribution)
+        {
+            foreach (var manager in managers)
+            {
+                var mail = new MailRequest
+                {
+                    ToEmail = manager.Email,
+                    Subject = "GRADING CONTRIBUTION",
+                    Body = $"There are some contributions which are not yet graded or do not have comments. To view more detail, see this link: {ungradedContributionUrl}"
+                };
+
+                await _emailService.SendEmailAsync(mail);
+            }
+        }
+        else
+        {
+            foreach (var manager in managers)
+            {
+                var mail = new MailRequest
+                {
+                    ToEmail = manager.Email,
+                    Subject = "GRADING CONTRIBUTION",
+                    Body = "All contribution has been comment by coordinators"
+                };
+
+                await _emailService.SendEmailAsync(mail);
+            }
+        }
+
+        _logger.LogInformation($"----- Email has been sent to all managers successfully ----- {_dateTimeProvider.UtcNow}");
+    }
+}
