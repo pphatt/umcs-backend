d283a1c20487b4c8798263067b0d31e71a239f46
pphatt <vutienphat0809@gmail.com>
2025-03-13 14:46:00 +0100

    test(faculty): add CQRS unit test for get faculty by id

---
+++ Server.Application.Tests/Faculties/Queries/GetFacultyById/GetFacultyByIdQueryHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+
+using Moq;
+
+using Server.Application.Common.Dtos.Content.Faculty;
+using Server.Application.Features.FacultyApp.Queries.GetFacultyById;
+using Server.Application.Wrapper;
+using Server.Domain.Common.Errors;
+using Server.Domain.Entity.Content;
+
+namespace Server.Application.Tests.Faculties.Queries.GetFacultyById;
+
+[Trait("Faculty", "Get Faculty By Id")]
+public class GetFacultyByIdQueryHandlerTests : BaseTest
+{
+    private readonly GetFacultyByIdQueryHandler _queryHandler;
+    private readonly Faculty _faculty;
+
+    public GetFacultyByIdQueryHandlerTests()
+    {
+        _faculty = new Faculty
+        {
+            Name = "IT"
+        };
+
+        _mockFacultyRepository
+            .Setup(repo => repo.GetByIdAsync(_faculty.Id))
+            .ReturnsAsync(_faculty);
+
+        _queryHandler = new GetFacultyByIdQueryHandler(_mockUnitOfWork.Object, _mapper);
+    }
+
+    [Fact]
+    public async Task GetFacultyByIdQueryHandler_GetFacultyById_Should_ReturnError_WhenSearchByFacultyIdCannotFound()
+    {
+        // Arrange
+        var query = new GetFacultyByIdQuery
+        {
+            Id = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await _queryHandler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().Be(Errors.Faculty.CannotFound);
+        result.FirstError.Code.Should().Be(Errors.Faculty.CannotFound.Code);
+        result.FirstError.Description.Should().Be(Errors.Faculty.CannotFound.Description);
+    }
+
+    [Fact]
+    public async Task GetFacultyByIdQueryHandler_GetFacultyById_ShouldQuerySuccessfully()
+    {
+        // Arrange
+        var query = new GetFacultyByIdQuery
+        {
+            Id = _faculty.Id
+        };
+
+        // Act
+        var result = await _queryHandler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsError.Should().BeFalse();
+        result.Value.Should().BeOfType<ResponseWrapper<FacultyDto>>();
+
+        var response = result.Value;
+        response.IsSuccessful.Should().BeTrue();
+        response.ResponseData.Should().NotBeNull();
+
+        response.ResponseData.Id.Should().Be(_faculty.Id);
+        response.ResponseData.Name.Should().Be(_faculty.Name);
+    }
+}
---
+++ Server.Application.Tests/Faculties/Queries/GetFacultyById/GetFacultyByIdQueryTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using Server.Application.Features.FacultyApp.Queries.GetFacultyById;
+using Server.Contracts.Faculties.GetFacultyById;
+
+namespace Server.Application.Tests.Faculties.Queries.GetFacultyById;
+
+[Trait("Faculty", "Get Faculty By Id")]
+public class GetFacultyByIdQueryTests : BaseTest
+{
+    [Fact]
+    public void GetFacultyByIdQuery_GetFacultyById_MapCorrectly()
+    {
+        // Arrange
+        var request = new GetFacultyByIdRequest
+        {
+            Id = Guid.NewGuid()
+        };
+
+        // Act
+        var result = _mapper.Map<GetFacultyByIdQuery>(request);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(request.Id);
+    }
+}
