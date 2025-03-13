5cc9e53a5960c5f19ec391476e4b0584d3477708
pphatt <vutienphat0809@gmail.com>
2025-03-13 14:10:00 +0100

    test: add MockQueryable dependency to mock UserManager

--- Server.Application.Tests/Faculties/Commands/DeleteFaculty/DeleteFacultyCommandHandlerTests.cs
+++ Server.Application.Tests/Faculties/Commands/DeleteFaculty/DeleteFacultyCommandHandlerTests.cs
@@ -1,19 +1,17 @@
-﻿using System.Linq.Expressions;
+﻿using FluentAssertions;
 
-using FluentAssertions;
-
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using MockQueryable;
 
 using Moq;
 
 using Server.Application.Features.FacultyApp.Commands.DeleteFaculty;
 using Server.Application.Wrapper;
 using Server.Domain.Common.Errors;
-using Server.Domain.Entity.Content;
 using Server.Domain.Entity.Identity;
 
 namespace Server.Application.Tests.Faculties.Commands.DeleteFaculty;
+
+using Faculty = Server.Domain.Entity.Content.Faculty;
 
 [Trait("Faculty", "Delete")]
 public class DeleteFacultyCommandHandlerTests : BaseTest
@@ -33,10 +31,11 @@ public class DeleteFacultyCommandHandlerTests : BaseTest
             .Setup(repo => repo.GetByIdAsync(_facultyId))
             .ReturnsAsync(faculty);
 
-        var users = new List<AppUser>().AsQueryable();
+        var users = new List<AppUser>();
+        var mockUsersQueryable = users.AsQueryable().BuildMock();
         _mockUserManager
             .Setup(m => m.Users)
-            .Returns(users);
+            .Returns(mockUsersQueryable);
 
         _commandHandler = new DeleteFacultyCommandHandler(_mockUnitOfWork.Object, _dateTimeProvider, _mockUserManager.Object);
     }
@@ -60,68 +59,64 @@ public class DeleteFacultyCommandHandlerTests : BaseTest
         result.FirstError.Description.Should().Be(Errors.Faculty.CannotFound.Description);
     }
 
-    // cannot mock any thing related to user manager (temporary).
-    //[Fact]
-    //public async Task DeleteFacultyCommandHandler_DeleteFaculty_Should_ReturnError_WhenHasUsersInFaculty()
-    //{
-    //    // Arrange
-    //    var command = new DeleteFacultyCommand
-    //    {
-    //        Id = _facultyId
-    //    };
+    [Fact]
+    public async Task DeleteFacultyCommandHandler_DeleteFaculty_Should_ReturnError_WhenHasUsersInFaculty()
+    {
+        // Arrange
+        var command = new DeleteFacultyCommand
+        {
+            Id = _facultyId
+        };
 
-    //    var user = new AppUser
-    //    {
-    //        Id = Guid.NewGuid(),
-    //        FacultyId = _facultyId,
-    //        UserName = "testuser",
-    //        Email = "test@example.com",
-    //        IsActive = true,
-    //        DateCreated = DateTime.UtcNow
-    //    };
+        var user = new AppUser
+        {
+            Id = Guid.NewGuid(),
+            FacultyId = _facultyId,
+            UserName = "testuser",
+            Email = "test@example.com",
+            IsActive = true,
+            DateCreated = DateTime.UtcNow
+        };
 
-    //    var usersList = new List<AppUser> { user };
-    //    var usersQueryable = usersList.AsQueryable();
+        var usersList = new List<AppUser> { user };
+        var mockUsersQueryable = usersList.AsQueryable().BuildMock();
+        _mockUserManager
+            .Setup(m => m.Users)
+            .Returns(mockUsersQueryable);
 
-    //    _mockUserManager
-    //        .Setup(m => m.Users)
-    //        .Returns(usersQueryable);
+        // Act
+        var result = await _commandHandler.Handle(command, CancellationToken.None);
 
-    //    // Act
-    //    var result = await _commandHandler.Handle(command, CancellationToken.None);
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().Be(Errors.Faculty.HasUserIn);
+        result.FirstError.Code.Should().Be(Errors.Faculty.HasUserIn.Code);
+        result.FirstError.Description.Should().Be(Errors.Faculty.HasUserIn.Description);
+    }
 
-    //    // Assert
-    //    result.IsError.Should().BeTrue();
-    //    result.FirstError.Should().Be(Errors.Faculty.HasUserIn);
-    //    result.FirstError.Code.Should().Be(Errors.Faculty.HasUserIn.Code);
-    //    result.FirstError.Description.Should().Be(Errors.Faculty.HasUserIn.Description);
-    //}
+    [Fact]
+    public async Task Handle_WhenValidRequest_ShouldDeleteSuccessfully()
+    {
+        // Arrange
+        var command = new DeleteFacultyCommand { Id = _facultyId };
 
-    //[Fact]
-    //public async Task Handle_WhenValidRequest_ShouldDeleteSuccessfully()
-    //{
-    //    // Arrange
-    //    var command = new DeleteFacultyCommand { Id = _facultyId };
+        var users = new List<AppUser>();
+        var mockUsersQueryable = users.AsQueryable().BuildMock();
+        _mockUserManager
+            .Setup(m => m.Users)
+            .Returns(mockUsersQueryable);
 
-    //    var users = new List<AppUser>().AsQueryable();
-    //    _mockUserManager
-    //        .Setup(m => m.Users)
-    //        .Returns(users);
+        // Act
+        var result = await _commandHandler.Handle(command, CancellationToken.None);
 
-    //    _mockUnitOfWork
-    //        .Setup(uow => uow.FacultyRepository.Update(It.IsAny<Faculty>()));
+        // Assert
+        result.IsError.Should().BeFalse();
+        result.Value.Should().BeOfType<ResponseWrapper>();
+        result.Value.IsSuccessful.Should().BeTrue();
+        result.Value.Message.Should().Be("Delete faculty successfully.");
 
-    //    // Act
-    //    var result = await _commandHandler.Handle(command, CancellationToken.None);
-
-    //    // Assert
-    //    result.IsError.Should().BeFalse();
-    //    result.Value.Should().BeOfType<ResponseWrapper>();
-    //    result.Value.IsSuccessful.Should().BeTrue();
-    //    result.Value.Message.Should().Be("Delete faculty successfully.");
-
-    //    _mockUnitOfWork.Verify(
-    //        uow => uow.CompleteAsync(),
-    //        Times.Once);
-    //}
+        _mockUnitOfWork.Verify(
+            uow => uow.CompleteAsync(),
+            Times.Once);
+    }
 }
