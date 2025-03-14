f35f4ddf8443c9db5b5300bdda47a75070ce6872
pphatt <vutienphat0809@gmail.com>
2025-03-14 14:22:00 +0100

    test(identity): add CQRS unit test for change user avatar

---
+++ Server.Application.Tests/Identity/Commands/ChangeUserAvatar/ChangeUserAvatarCommandValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.TestHelper;
+
+using Microsoft.AspNetCore.Http;
+
+using Moq;
+
+using Server.Application.Features.Identity.Commands.ChangeUserAvatar;
+
+namespace Server.Application.Tests.Identity.Commands.ChangeUserAvatar;
+
+[Trait("Identity", "Change User Avatar")]
+public class ChangeUserAvatarCommandValidatorTests : BaseTest
+{
+    private readonly ChangeUserAvatarCommandValidator _validator;
+
+    public ChangeUserAvatarCommandValidatorTests()
+    {
+        _validator = new ChangeUserAvatarCommandValidator();
+    }
+
+    [Fact]
+    public async Task ChangeUserAvatarCommandValidator_ShouldNot_ReturnError_WhenCommandIsValid()
+    {
+        // Arrange
+        var command = new ChangeUserAvatarCommand
+        {
+            UserId = Guid.NewGuid(),
+            Avatar = new Mock<IFormFile>().Object
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
