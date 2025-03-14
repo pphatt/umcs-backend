ed69b2cbe91f36b38dacb1a7256bebde9d84351b
pphatt <vutienphat0809@gmail.com>
2025-03-14 04:20:00 +0100

    test(identity): add CQRS unit test for delete user

---
+++ Server.Application.Tests/Identity/Commands/DeleteUser/DeleteUserCommandValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.TestHelper;
+
+using Server.Application.Features.Identity.Commands.DeleteUser;
+
+namespace Server.Application.Tests.Identity.Commands.DeleteUser;
+
+[Trait("Identity", "Delete")]
+public class DeleteUserCommandValidatorTests : BaseTest
+{
+    private readonly DeleteUserCommandValidator _validator;
+
+    public DeleteUserCommandValidatorTests()
+    {
+        _validator = new DeleteUserCommandValidator();
+    }
+
+    [Fact]
+    public async Task DeleteUserCommandValidator_ShouldNot_ReturnError_WhenCommandIsValid()
+    {
+        // Arrange
+        var command = new DeleteUserCommand
+        {
+            Id = Guid.NewGuid()
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
