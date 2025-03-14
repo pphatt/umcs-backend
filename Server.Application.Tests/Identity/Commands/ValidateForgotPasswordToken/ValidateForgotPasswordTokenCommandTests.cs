b01169b7eb38a12d275974d9380f5af50edb87d9
pphatt <vutienphat0809@gmail.com>
2025-03-14 14:02:00 +0100

    test(identity): add CQRS unit test for validate forgot password token

---
+++ Server.Application.Tests/Identity/Commands/ValidateForgotPasswordToken/ValidateForgotPasswordTokenCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using Server.Application.Features.Identity.Commands.ValidateForgotPasswordToken;
+using Server.Contracts.Identity.ValidateForgotPasswordToken;
+
+namespace Server.Application.Tests.Identity.Commands.ValidateForgotPasswordToken;
+
+[Trait("Identity", "Validate Forgot Password Token")]
+public class ValidateForgotPasswordTokenCommandTests : BaseTest
+{
+    [Fact]
+    public void ValidateForgotPasswordTokenCommand_ValidateForgotPasswordToken_MapCorrectly()
+    {
+        // Arrange
+        var request = new ValidateForgotPasswordTokenRequest
+        {
+            Token = ""
+        };
+
+        // Act
+        var result = _mapper.Map<ValidateForgotPasswordTokenCommand>(request);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Token.Should().Be(request.Token);
+    }
+}
---
+++ Server.Application.Tests/Identity/Commands/ValidateForgotPasswordToken/ValidateForgotPasswordTokenCommandValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.TestHelper;
+
+using Server.Application.Features.Identity.Commands.ValidateForgotPasswordToken;
+
+namespace Server.Application.Tests.Identity.Commands.ValidateForgotPasswordToken;
+
+[Trait("Identity", "Validate Forgot Password Token")]
+public class ValidateForgotPasswordTokenCommandValidatorTests : BaseTest
+{
+    private readonly ValidateForgotPasswordTokenCommandValidator _validator;
+
+    public ValidateForgotPasswordTokenCommandValidatorTests()
+    {
+        _validator = new ValidateForgotPasswordTokenCommandValidator();
+    }
+
+    [Fact]
+    public async Task ValidateForgotPasswordTokenCommandValidator_ShouldNot_ReturnError_WhenCommandIsValid()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var token = CreateToken(userId);
+        var command = new ValidateForgotPasswordTokenCommand
+        {
+            Token = Uri.EscapeDataString(token),
+        };
+
+        // Act
+        var result = await _validator.TestValidateAsync(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    private string CreateToken(string userId)
+    {
+        using (var ms = new MemoryStream())
+        {
+            using (var writer = new BinaryWriter(ms))
+            {
+                writer.Write(DateTimeOffset.UtcNow.Ticks);
+                writer.Write(userId);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+    }
+}
