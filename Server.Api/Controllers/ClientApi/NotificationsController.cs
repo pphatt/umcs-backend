cbfee29e9c78a935ffb3ece74d777fcd93afcc5e
pphatt <vutienphat080903.dev@gmail.com>
2025-03-25 08:53:04 +0700

    feat(notification): add controller

---
+++ Server.Api/Controllers/ClientApi/NotificationsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Server.Application.Common.Extensions;
+using Server.Application.Features.Notification.Queries.GetAllUserNotificationsPagination;
+using Server.Contracts.Notifications.GetAllUserNotificationsPagination;
+
+namespace Server.Api.Controllers.ClientApi;
+
+public class NotificationsController : ClientApiController
+{
+    private readonly IMapper _mapper;
+
+    public NotificationsController(IMapper mapper, ISender mediatorSender) : base(mediatorSender)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpGet("pagination")]
+    public async Task<IActionResult> GetAllUsersNotificationsPagination([FromQuery] GetAllUserNotificationsPaginationRequest request)
+    {
+        var mapper = _mapper.Map<GetAllUserNotificationsPaginationQuery>(request);
+
+        mapper.UserId = User.GetUserId();
+
+        var result = await _mediatorSender.Send(mapper);
+
+        return result.Match(
+            paginationResult => Ok(paginationResult),
+            errors => Problem(errors)
+        );
+    }
+}
