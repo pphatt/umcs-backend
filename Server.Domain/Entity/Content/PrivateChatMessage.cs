3772f8b6b0ee1f5c0f7c92bb89d8e038f5ffb8f1
pphatt <vutienphat080903.dev@gmail.com>
2025-03-25 16:36:25 +0700

    feat(migration): add PrivateChatRoom and PrivateChatMessage

---
+++ Server.Domain/Entity/Content/PrivateChatMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Domain.Entity.Content;
+
+[Table("PrivateChatMessages")]
+public class PrivateChatMessage : BaseEntity
+{
+    public Guid SenderId { get; set; }
+    public Guid ReceiverId { get; set; }
+    public string Message { get; set; } = default!;
+    public bool HasRed { get; set; }
+}
---
+++ Server.Domain/Entity/Content/PrivateChatRoom.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Domain.Entity.Content;
+
+[Table("PrivateChatRooms")]
+public class PrivateChatRoom : BaseEntity
+{
+    public Guid User1Id { get; set; }
+    public Guid User2Id { get; set; }
+    public Guid User2LastActivity { get; set; }
+}
