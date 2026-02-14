10ca526709c95496bdc118d1e9dc8d1883ebf563
pphatt <phatvutien.dev@gmail.com>
2026-02-14 17:40:05 +0700

    test wsl

---
+++ Server.Api/Controllers/TestApi/TestApiControllerDockerSample.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Api.Controllers.TestApi;
+
+[Tags("Test")]
+public class TestApiControllerDockerSample : TestApiController
+{
+    [HttpGet("test-docker-compose-up")]
+    public IActionResult TestDockerComposeUp()
+    {
+        return Ok("Docker compose up successfully.");
+    }
+
+    [HttpGet("test-docker-compose-up-build")]
+    public IActionResult TestDockerComposeUpBuild()
+    {
+        return Ok("Docker compose up successfully.");
+    }
+}
